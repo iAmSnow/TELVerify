5477fed7444e51ffed79ec3b3a83880124500bde
iAmSnow <toom_sarawoot@hotmail.com>
2019-03-03 15:03:37 +0700

    sadasd

--- TELVerify/Home.swift
+++ TELVerify/Home.swift
@@ -8,12 +8,17 @@
 
 import Foundation
 import TELVerifyFramework
-import Alamofire
+
 public class Home {
     
-    public static func callAPI() {
-        Alamofire.request("https://httpbin.org/get").responseJSON { (res) in
-            print(res.value)
-        }
+    /// Login Test Service
+    ///
+    /// - Parameters:
+    ///   - email: sale@gmail.com
+    ///   - password: 123456
+    public static func LoginTest(email: String, password: String) {
+      
+        TELVerify.shared.service.login(email: email, password: password)
+        
     }
 }
